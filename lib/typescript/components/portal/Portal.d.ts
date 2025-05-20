45ff44e33a3c14a941cb94231b67b26253a8877b
AgustinMJ <agustinmillanjimenez+github@gmail.com>
2025-05-20 09:54:20 +0200

    chore: add `lib` to git

---
+++ lib/typescript/components/portal/Portal.d.ts
@@ -0,0 +1,3 @@
+import React from 'react';
+import type { PortalProps } from './types';
+export declare const Portal: React.MemoExoticComponent<({ name: _providedName, hostName, handleOnMount: _providedHandleOnMount, handleOnUnmount: _providedHandleOnUnmount, handleOnUpdate: _providedHandleOnUpdate, children, }: PortalProps) => null>;
